f7876e73c8e28f23fa818a276050fc50589f9616
TheWudu <mfuehrlinger@gmx.at>
2022-06-11 19:13:16 +0200

    Add duration and elevation, and some formating to sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -23,7 +23,9 @@ class SessionsController < ApplicationController
       session.merge(
         start_time: session['start_time'].in_time_zone(session['timezone']),
         sport: SportType.for(id: session['sport_type_id']),
-        distance: (session['distance'] / 1000.0).round(2)
+        distance: format_distance(session['distance']),
+        duration: format_ms(session['duration']),
+        elevation: format_elevation(session['elevation_gain'], session['elevation_loss'])
       )
     end
   end
@@ -48,5 +50,26 @@ class SessionsController < ApplicationController
       sport_type_ids: sport_type_ids,
       group_by: group_by
     )
+  end
+
+  def format_ms(millis)
+    secs, = millis.divmod(1000) # divmod returns [quotient, modulus]
+    mins, secs = secs.divmod(60)
+    hours, mins = mins.divmod(60)
+    hours = nil if hours.zero?
+
+    [hours, mins, secs].compact.map { |e| e.to_s.rjust(2, '0') }.join ':'
+  end
+
+  def format_distance(distance)
+    return '-' if !distance || distance.zero?
+
+    (distance / 1000.0).round(2)
+  end
+
+  def format_elevation(gain, loss)
+    return '-' if gain.zero? && loss.zero?
+
+    "#{gain} / #{loss}"
   end
 end
