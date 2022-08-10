835f1be716d01b432daacd0bd0329d365592083a
TheWudu <mfuehrlinger@gmx.at>
2022-08-10 18:01:32 +0200

    Also show rectangle now in the map on trace matcher

--- lib/use_cases/traces/matcher.rb
+++ lib/use_cases/traces/matcher.rb
@@ -9,7 +9,8 @@ module UseCases
       MIN_OVERLAP = 60 # %
 
       attr_accessor :trace1, :trace2, :blocks1, :blocks2, :match_in_percent,
-                    :block_size, :min_overlap, :orig1, :orig2
+                    :block_size, :min_overlap, :orig1, :orig2, 
+                    :lat_lng_blocks1, :lat_lng_blocks2
 
       def initialize(trace1:, trace2:, block_size: nil, min_overlap: nil)
         self.trace1 = trace1
@@ -25,6 +26,10 @@ module UseCases
         self.blocks2 = trace2.map { |p| blockify(p) }.uniq
         self.orig1   = trace1.map { |p| blockify(p, 1) }
         self.orig2   = trace2.map { |p| blockify(p, 1) }
+
+
+        self.lat_lng_blocks1 = bounds(blocks1) 
+        self.lat_lng_blocks2 = bounds(blocks2) 
 
         diffs = find_matches_v2
         self.match_in_percent = ((diffs.count(true).to_f / diffs.count) * 100).round(2)
@@ -50,6 +55,25 @@ module UseCases
 
       def calc(val)
         (6370 * Math::PI * val / 180 * 1000)
+      end
+
+      def uncalc(val)
+        (val * 180 / 6370 / Math::PI / 1000)
+      end
+
+      def to_lat_lng(block)
+        lng1 = uncalc(block[0])
+        lng2 = uncalc(block[0] + block_size)
+        lat1 = uncalc(block[1])
+        lat2 = uncalc(block[1] + block_size)
+
+        [[lat1, lng1],[lat2, lng2]]
+      end
+
+      def bounds(blocks)
+        blocks.map do |b|
+          to_lat_lng(b)
+        end
       end
 
       def blockify(point, bs = block_size)
