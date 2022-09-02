370b9a936a71a4e93cead2cdbe737da44a9c7dd3
TheWudu <mfuehrlinger@gmx.at>
2022-09-02 14:22:27 +0200

    Enhance pause detection

---
+++ spec/lib/parser/gpx/pause_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe Parser::Gpx do
+  let(:data) { File.read(input_file) }
+
+  let(:parser) { described_class.new(data: data) }
+  subject { parser.parse }
+
+  context 'with running session' do
+    let(:input_file) { 'spec/fixtures/traces/running_8km.gpx' }
+
+    # has a pause of 02:29 (149 s) regarding runtastic
+    # it { expect(subject.first[:pause]).to eq(149000) }
+    it { expect(subject.first[:pause]).to eq(110000) }
+  end
+
+  context 'with a hiking session' do
+    let(:input_file) { 'spec/fixtures/traces/hiking_20220830_gradlspitz.gpx' }
+
+    # has a pause of 17:58 (1078 s) regarding runtastic 
+    it { expect(subject.first[:pause]).to eq(1105000) }
+  end
+end
